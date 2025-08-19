f5726b8e9256de1bc3fe799ba580f6e939766759
Mehedi Hasan <ug2102007@cse.pstu.ac.bd>
2025-08-19 12:24:15 +0600

    Example 6.8 Write a count-controlled loop to display a row of 80 stars.

--- mycode.asm
+++ mycode.asm
@@ -1,1 +1,12 @@
-;Example 6.8 Write a count-controlled loop to display a row of 80 stars.
+;Example 6.8 Write a count-controlled loop to display a row of 80 stars.       
+org 100h
+
+mov cx, 80
+mov ah, 2
+mov dl, '*'
+
+display_loop:
+int 21h
+loop display_loop
+
+ret
